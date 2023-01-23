b8f8bfdd8d3172ac13d312d4ebcb4f807fcefde1
pheak <kunnew20@gmail.com>
2023-01-23 08:45:14 +0700

    update

--- server.js
+++ server.js
@@ -27,10 +27,7 @@ connectDB()
 
 const port = process.env.PORT ||  5000
 
-// app.get('/api/',(req,res) => {
 
-//     res.json({message: 'API is running'})
-// })
 
 app.use(`/api/auth`,authRoutes)
 app.use(`/api/jobs`,jobRoutes)
@@ -43,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, './frontend/dist', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
